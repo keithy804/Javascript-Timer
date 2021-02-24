69a36182785a897b31ca6448efb01c13f6d85efa
keithy804 <63236735+keithy804@users.noreply.github.com>
2021-02-24 10:47:25 +0000

    Update timer.js

--- timer.js
+++ timer.js
@@ -67,7 +67,3 @@ function calculateFutureDate (dateTochange){
 		timer(weeklyDate);// sends it to the timer function
 		//console.log("new: "+dateTochange);		
 }
-// liveStream function changes the webpage to the webpage where the live stream is hosted
-function liveStream (){
-	window.location.assign("http://www.clcconthemove.org/clccLivestream.html");
-}
